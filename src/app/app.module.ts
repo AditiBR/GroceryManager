52fd6a9e3d61872612f3f6bff9d55f34071b15da
AditiBR <53121538+AditiBR@users.noreply.github.com>
2020-03-04 19:19:05 -0800

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppGroceryComponent } from './app-grocery/app-grocery.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppGroceryComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
