f08250b6d6f428026deb4629070ab371210c9479
Aditi R <aditi.seattle@gmail.com>
2020-03-12 00:57:44 -0700

    Added bootstrap and Add/Edit/Delete functionality

--- src/app/app-grocery/app-grocery.component.ts
+++ src/app/app-grocery/app-grocery.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-app-grocery',
   templateUrl: './app-grocery.component.html',
-  styleUrls: ['./app-grocery.component.css']
+  // styleUrls: ['./app-grocery.component.css']
+  styleUrls: ['../../../src/assets/bootstrap/css/bootstrap.min.css']
 })
 export class AppGroceryComponent implements OnInit {
 
-  task: string;
+  task = {
+    name:'',
+    id:0
+  };
   tasks =[];
   constructor() { }
 
@@ -16,8 +21,52 @@ export class AppGroceryComponent implements OnInit {
 
   onClick()
   {
-    this.tasks.push({name: this.task });
-    this.task='';
+    if(this.task.id === 0){
+      this.tasks.push({
+        id: (new Date()).getTime(),
+        name: this.task.name,
+        strike: false
+        });
+    }
+
+    this.task = {
+      name: '',
+      id: 0
+  };
+
+    // this.tasks.push({name: this.task });
+    // this.task='';
+  }
+
+  onEdit(item){
+  this.task=item;
+  }
+
+  onDelete(item){
+    for(var i=0;i<this.tasks.length;i ++){
+      if(this.tasks[i].id == item.id){
+        this.tasks.splice(i,1);
+        break;
+      }
+
+    }
+  }
+
+  onStrike(item)
+  {
+    for(var i=0;i<this.tasks.length;i++){
+      if(this.tasks[i].id == item.id){{
+        if(this.tasks[i].strike){
+          this.tasks[i].strike = false;
+        } else {
+          this.tasks[i].strike = true;
+        }
+        break;
+      }
+
+      }
+    }
+
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppGroceryComponent } from './app-grocery/app-grocery.component';
 
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+import 'hammerjs'; //Delete??
 
 @NgModule({
   declarations: [
@@ -13,7 +24,16 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
