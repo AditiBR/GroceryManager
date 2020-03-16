c5b6ef4eacabb32e6f2432655db2fcc11dc457a0
Aditi R <aditi.seattle@gmail.com>
2020-03-15 18:04:00 -0700

    Added validation when user has an empty string, when user tries to enter an item alreday in the list

--- src/app/app-grocery/app-grocery.component.ts
+++ src/app/app-grocery/app-grocery.component.ts
@@ -1,81 +1,101 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-app-grocery',
-  templateUrl: './app-grocery.component.html',
-  // styleUrls: ['./app-grocery.component.css']
-  styleUrls: ['../../../src/assets/bootstrap/css/bootstrap.min.css']
+  selector: 'app-app-grocery',
+  templateUrl: './app-grocery.component.html',
+  styleUrls: ['../../../src/assets/bootstrap/css/bootstrap.min.css']
 })
-export class AppGroceryComponent implements OnInit {
+export class AppGroceryComponent implements OnInit {
+  task = {
+    name:'',
+    id:0
+  };
+  tasks =[];
+  constructor() { }
 
-  task = {
-    name:'',
-    id:0
-  };
-  tasks =[];
-  constructor() { }
+  ngOnInit(): void {
+  }
 
-  ngOnInit(): void {
-  }
+  onClick()
+  {
+   // Validation if there are only blank spaces nothing gets added to the list
+    if(!this.task.name.trim())
+    {
+      this.task = {
+        name: '',
+        id: 0
+    };  
+      return;
+    }   
+  
+   // Add first item to the list
+    if(this.tasks.length == 0){
+      this.tasks.push({
+        id: (new Date()).getTime(),
+        name: this.task.name,
+        strike: false
+        });
+        this.task = {
+          name: '',
+          id: 0
+      };
+    
+        return;
+    }
 
-  onClick()
-  {
-    if(!this.task.name.trim())
-    {
-      return;
-    }
-    if(this.tasks.includes(this.task.name))
-    {
-      
-    }
-    if(this.task.id === 0){
-      this.tasks.push({
-        id: (new Date()).getTime(),
-        name: this.task.name,
-        strike: false
-        });
-    }
+    // If list has any items check if entered item is alreday there in the list, allow to add that item only if it is marked as done previously if it is not marked as complete do not add that item as it is still there in the list
+    var addtoList = true;    
+  
+    for(var i=0;i<this.tasks.length;i ++){
+        if(this.tasks[i].name.toLowerCase() == this.task.name.toLowerCase()){
+              if(!this.tasks[i].strike)
+              {
+                  addtoList= false;
+              }           
+        }         
+      }
+      if(addtoList){
+          this.tasks.push({
+                        id: (new Date()).getTime(),
+                        name: this.task.name,
+                         strike: false
+                      });
+      } 
+    this.task = {
+      name: '',
+      id: 0
+    };    
+  }
 
-    this.task = {
-      name: '',
-      id: 0
-  };
+  //Edit an existing item in the list
+  onEdit(item){
+  this.task=item;
+  }
 
-    // this.tasks.push({name: this.task });
-    // this.task='';
-  }
+  //Delete an existing item in the list
+  onDelete(item){
+    for(var i=0;i<this.tasks.length;i ++){
+      if(this.tasks[i].id == item.id){
+        this.tasks.splice(i,1);
+        this.task.name='';
+        break;
+      }      
+    }
+  }
 
-  onEdit(item){
-  this.task=item;
-  }
-
-  onDelete(item){
-    for(var i=0;i<this.tasks.length;i ++){
-      if(this.tasks[i].id == item.id){
-        this.tasks.splice(i,1);
-        this.task.name='';
-        break;
-      }
-      
-    }
-  }
-
-  onStrike(item)
-  {
-    for(var i=0;i<this.tasks.length;i++){
-      if(this.tasks[i].id == item.id){{
-        if(this.tasks[i].strike){
-          this.tasks[i].strike = false;
-        } else {
-          this.tasks[i].strike = true;
-        }
-        break;
-      }
-
-      }
-    }
-
-  }
-
+ // Mark item as complete
+  onStrike(item)
+  {
+    for(var i=0;i<this.tasks.length;i++){
+      if(this.tasks[i].id == item.id){{
+        if(this.tasks[i].strike){
+          this.tasks[i].strike = false;
+        } else {
+          this.tasks[i].strike = true;
+        }
+        break;
+      }
+      }
+    }
+  }
 }
